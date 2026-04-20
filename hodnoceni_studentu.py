a6dad78023dd78cf079f8f761748896b946f2dc4
David <279353@vutbr.cz>
2026-04-20 14:11:51 +0200

    Update

--- hodnoceni_studentu.py
+++ hodnoceni_studentu.py
@@ -24,6 +24,27 @@ class StudentsGrades:
         else:
             return "F"
 
+    def find(self, points):
+        positions = []
+        for i, score in enumerate(self.scores):
+            if score == points:
+                positions.append(i)
+        return positions
+
+
+    def get_sorted(self):
+        scores = self.scores.copy()
+        p = len(scores)
+        for i in range(p):
+            swapped = False
+            for j in range(0, p - i - 1):
+                if scores[j] > scores[j + 1]:
+                    scores[j], scores[j + 1] = scores[j + 1], scores[j]
+                    swapped = True
+                    if not swapped:
+                        break
+        return scores
+
 
 
 
@@ -32,6 +53,14 @@ results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
 print(results.count())          # 9
 print(results.get_by_index(2))  # 91
 print(results.scores)           # [85, 42, 91, 67, 50, 73, 100, 38, 58]
+
+print(results.get_grade(2))  # A (91 bodů)
+print(results.get_grade(6))  # A (100 bodů)
+print(results.get_grade(7))  # F (38 bodů)
+
+print(results.find(100))  # [6]
+print(results.find(50))   # [4]
+print(results.find(77))   # []
 
 print(results.get_grade(2))  # A (91 bodů)
 print(results.get_grade(6))  # A (100 bodů)
