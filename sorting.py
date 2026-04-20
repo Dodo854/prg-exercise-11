177efae53035fa26643c3aaaf73282aaad06d809
David <279353@vutbr.cz>
2026-04-20 13:28:00 +0200

    Update

--- sorting.py
+++ sorting.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -17,12 +19,46 @@ def selection_sort(values):
         p += 1
     return values
 
+def bubble_sort(values):
+    values = values[:]
+    p = len(values)
+
+    plt.ion()
+    plt.show()
+
+    for i in range(p):
+        swapped = False
+
+        for j in range(0, p - i -1):
+
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+
+            if values[j] > values[j + 1]:
+                values[j], values[j + 1] = values[j + 1], values[j]
+                swapped = True
+
+                if not swapped:
+                    break
+    return values
+
+
 
 def main():
-    values = random_numbers(10)  # 10 čísel v rozsahu 0–100
-    sorted = selection_sort(values)
-    print(sorted)
-    small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
+    values = random_numbers(100)
+    sorted_by_piece = selection_sort(values)
+    print(sorted_by_piece)
+
+    bubbles = bubble_sort(values)
+    print(bubbles)
+
 
 if __name__ == "__main__":
     main()
